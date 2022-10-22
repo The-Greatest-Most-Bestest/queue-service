7cc6e48d4fc4246f82146bff934791aa64ef1709
Riley Kuttruff <72955101+RKuttruff@users.noreply.github.com>
2022-10-21 19:52:47 -0700

    Initial structure (#22)
    * Some files and a pytest suite as a standin for unit tests
    
    * HTTP GET should be mocked out in UT
    
    * Some basic actual code similar to our project

---
+++ src/queueservice/MongoDBProxy.py
@@ -0,0 +1,12 @@
+
+
+
+class MongoDBProxy:
+    def __init__(self, url):
+        self.url = url
+
+    def query(self, query_string):
+        pass
+
+    def update(self, query_string, update_item):
+        pass
