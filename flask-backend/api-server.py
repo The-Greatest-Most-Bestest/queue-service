997ea57ee770a233c8b39a74b10a38cb7e937f56
leolanggeng <91353349+leolanggeng@users.noreply.github.com>
2022-10-31 11:25:57 -0700

    sample code for email notif

--- flask-backend/api-server.py
+++ flask-backend/api-server.py
@@ -1,11 +1,19 @@
+import string
 from flask import Flask
 from pymongo import MongoClient
+
+import datetime
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 app = Flask(__name__)
 
 # PyMongo Stuff
-client = MongoClient("mongodb+srv://mfngoi:UUkHZnMzVW6x9jIo@cluster0.ltgs7ps.mongodb.net/?retryWrites=true&w=majority")
-db = client.queues
+client = MongoClient("mongodb+srv://langgeng:vv8q1OJCO3ANbe9b@clusterfuck.9jonez9.mongodb.net/?retryWrites=true&w=majority")
+db = client.queueDB
+
+
 
 # API Route
 @app.route("/")
@@ -20,5 +28,60 @@ def team():
 def queue():
     return db.list_collection_names()
 
+@app.route("/push/<collection>/<fname>/<lname>/<broncoID>")
+def test(collection, fname, lname, broncoID):
+    coll = db[collection]
+    seqID = coll.countDocuments({})
+    print(seqID)
+    coll.insert_one(
+        {
+            'seqID' : seqID,
+            'fname' : fname,
+            'lname' : lname,
+            'broncoID' : broncoID,
+            'time' : datetime.datetime.now()
+        }
+    )
+    return "pushedUser"
+
+@app.route("/email")
+def email():
+    receiver = '130nardolanggeng@gmail.com'
+    location = 'BSC Games Room'
+    queue_name = 'PS4 Console'
+    sendEmail(receiver, location, queue_name)
+    return('email sent')
+
+def sendEmail(send_to, location, queue_name):
+    subject = 'It is your turn for ' + queue_name + ' at ' + location
+    mail_content = 'Mail content yada yada yada. \n Ⓒ GMB 2022.'
+
+    sender_address = 'gmbcs4800@gmail.com'
+    sender_pass = 'rbrqwjrgxonlzsli'
+    receiver_address = send_to
+
+    #Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = subject   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
+
+    return 'email sent check'
+
+
 if __name__ == "__main__":
     app.run()
+
+
+
+
